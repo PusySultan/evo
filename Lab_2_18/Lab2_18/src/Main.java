29a08956e882897cd46104621e9fde261009c3e7
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2025-02-26 00:36:26 +0300

    fixed uncorrect output

--- Lab_2_18/Lab2_18/src/Main.java
+++ Lab_2_18/Lab2_18/src/Main.java
@@ -7,11 +7,10 @@ public class Main
         String name = "";
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Computer: Привет, как тебя зовут? ");
-        System.out.print("You: ");
+        System.out.println("Привет, как тебя зовут? ");
 
         name = sc.next();
-        System.out.println("Computer: Привет, " + name + "!!!");
+        System.out.println("Привет, " + name + "!");
         sc.close();
     }
 }

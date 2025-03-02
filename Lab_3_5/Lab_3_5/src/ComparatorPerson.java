a20890ff997f86abe521146e98e0835bc0b3fe1c
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2025-03-02 13:36:19 +0300

    Lab 3_5

---
+++ Lab_3_5/Lab_3_5/src/ComparatorPerson.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class ComparatorPerson implements Comparator<User>
+{
+    @Override
+    public int compare(User o1, User o2)
+    {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+}

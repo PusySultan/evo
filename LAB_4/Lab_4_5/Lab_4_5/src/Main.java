d997223bd2745f9377f0188ac361418ac19236d4
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2025-03-12 23:19:17 +0300

    Lab 4_5

--- LAB_4/Lab_4_5/Lab_4_5/src/Main.java
+++ LAB_4/Lab_4_5/Lab_4_5/src/Main.java
@@ -1,9 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.stream.LongStream;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        System.out.println(getArithmeticProgressionSum(10_000_000, 1_000_000_000));
+    }
+
+    public static long getArithmeticProgressionSum(int a, int b)
+    {
+        return LongStream
+                    .range(a, b)
+                    .sum();
     }
 }
